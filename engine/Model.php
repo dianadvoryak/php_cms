8e77ac43ad1d42a7e28892366fd77454b64d7ed3
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-05-14 22:44:34 +0200

    create model, entity, repository, activeRecord, trait

--- engine/Model.php
+++ engine/Model.php
@@ -2,24 +2,22 @@
 
 namespace Engine;
 
-use Engine\DI\DI;
+use Engine\Core\Database\QueryBuilder;
+use engine\DI\DI;
 
-abstract class Controller
+abstract class Model
 {
     protected $di;
     protected $db;
-    protected $view;
     protected $config;
-    protected $request;
-    protected $load;
+    protected $queryBuilder;
 
     public function __construct(DI $di)
     {
         $this->di = $di;
         $this->db = $this->di->get('db');
-        $this->view = $this->di->get('view');
         $this->config = $this->di->get('config');
-        $this->request = $this->di->get('request');
-        $this->load = $this->di->get('load');
+
+        $this->queryBuilder = new QueryBuilder();
     }
 }
